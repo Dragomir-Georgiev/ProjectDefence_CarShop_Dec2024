3496002dcbea51d060a7110f07f2f00044d7cc5a
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-10-29 14:11:10 +0200

    Added feedback/comment table for user review of the car

---
+++ CarShop.Data.Models/FeedBack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.Data.Models
+{
+    public class FeedBack
+    {
+        public Guid Id { get; set; }
+        //TODO ADD USER 
+
+        public Guid CarId { get; set; }
+        public Car Car { get; set; }
+
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+        public DateTime FeedbackDate { get; set; }
+    }
+}
