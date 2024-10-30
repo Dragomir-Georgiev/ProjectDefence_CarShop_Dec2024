2b805005c29f42181f271a34315beebd08230011
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-10-30 13:47:33 +0200

    Added a Discount table

---
+++ CarShop.Data.Models/Discount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.Data.Models
+{
+    public class Discount
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public double DiscountPercentage { get; set; }
+    }
+}
