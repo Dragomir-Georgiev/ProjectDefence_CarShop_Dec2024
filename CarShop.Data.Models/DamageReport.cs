9463b3dab41528bd9d1e7f8fb36ffe13678e9025
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-10-30 13:43:29 +0200

    Added DamageReport table

---
+++ CarShop.Data.Models/DamageReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.Data.Models
+{
+    public class DamageReport
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public DateTime ReportedDate { get; set; }
+        public decimal CostEstimation { get; set; }
+        public Guid CarId { get; set; }
+        public Car Car { get; set; }
+    }
+}
