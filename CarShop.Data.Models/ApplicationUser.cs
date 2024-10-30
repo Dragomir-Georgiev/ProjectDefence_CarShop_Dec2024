acc2c3f65b43116fe7fd86b8505055bc293766e0
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-10-30 13:02:40 +0200

    Fixed dependencies and added a user table

---
+++ CarShop.Data.Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CarShop.Data.Models
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public ApplicationUser() 
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
