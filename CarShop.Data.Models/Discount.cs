a05fd9d8d62b2f0c3fd436d00a94554bb9330da9
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-10-31 13:53:37 +0200

    Updated discount properties with data annotations

--- CarShop.Data.Models/Discount.cs
+++ CarShop.Data.Models/Discount.cs
@@ -1,15 +1,24 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using static CarShop.Common.EntityValidationConstants.Discount;
 namespace CarShop.Data.Models
 {
     public class Discount
     {
+        [Key]
+        [Comment("Discount unique identifier")]
         public Guid Id { get; set; }
-        public string Description { get; set; }
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        [Comment("Description of the discount")]
+        public string Description { get; set; } = null!;
+        [Required]
+        [Comment("Percentage of the discount for the daily price of the car")]
         public double DiscountPercentage { get; set; }
     }
 }
