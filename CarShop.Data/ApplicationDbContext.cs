3742d9e910c63435305378abb9bdfe66cf4d5a3d
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-10-31 14:17:04 +0200

    Added all dbset to context

--- CarShop.Data/ApplicationDbContext.cs
+++ CarShop.Data/ApplicationDbContext.cs
@@ -2,14 +2,32 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace CarShop.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
+        public ApplicationDbContext()
+        {
+        }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<ApplicationUser> ApplicationsUsers { get; set; }
+        public DbSet<CarCategory> CarCategories { get; set; }
+        public DbSet<CarDiscount> CarDiscounts { get; set; }
+        public DbSet<DamageReport> DamageReports { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
