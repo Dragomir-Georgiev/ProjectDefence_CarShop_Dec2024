bf0834c653ec0d4e670cefe577233732f2adb3cb
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-10-31 14:06:03 +0200

    Updated rental properties with data annotations

--- CarShop.Data.Models/Rental.cs
+++ CarShop.Data.Models/Rental.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +11,21 @@ namespace CarShop.Data.Models
 {
     public class Rental
     {
+        [Key]
+        [Comment("Rental unique identifier")]
         public Guid Id { get; set; }
         public Guid ApplicationUserId { get; set; }
-        public ApplicationUser ApplicationUser { get; set; }
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser ApplicationUser { get; set; } = null!;
+
         public Guid CarId { get; set; }
-        public Car Car { get; set; }
+        [ForeignKey(nameof(CarId))]
+        public Car Car { get; set; } = null!;
+        [Required]
         public DateTime StartDate { get; set; }
+        [Required]
         public DateTime EndDate { get; set; }
+        [Required]
         public decimal TotalCost { get; set; }
     }
 }
