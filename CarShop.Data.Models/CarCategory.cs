c197596a8bbec4506972d2ec9626fa39d7664ea5
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-10-31 13:05:58 +0200

    Updated CarCategory properties with data annotations

--- CarShop.Data.Models/CarCategory.cs
+++ CarShop.Data.Models/CarCategory.cs
@@ -5,14 +5,20 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using static CarShop.Common.EntityValidationConstants.CarCategory;
 
 namespace CarShop.Data.Models
 {
     public class CarCategory
     {
+        [Key]
+        [Comment("Car category unique identifier")]
         public Guid Id { get; set; }
-        public string CategoryName { get; set; }
-        public string Description { get; set; }
+        [Required]
+        [MaxLength(NameMaxLength)]
+        [Comment("The name of the category")]
+        public string CategoryName { get; set; } = null!;
         public ICollection<Car> Cars { get; set; } = new HashSet<Car>();
     }
 }
