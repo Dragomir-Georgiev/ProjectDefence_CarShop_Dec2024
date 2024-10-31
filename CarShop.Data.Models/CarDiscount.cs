ade1719e3ee662fd29f41d6a3ea657071e309aad
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-10-31 13:16:22 +0200

    Updated CarDiscount properties with data annotations

--- CarShop.Data.Models/CarDiscount.cs
+++ CarShop.Data.Models/CarDiscount.cs
@@ -1,16 +1,25 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CarShop.Data.Models
 {
+    [PrimaryKey(nameof(CarId), nameof(DiscountId))]
     public class CarDiscount
     {
+        [Comment("Car unique identifier")]
         public Guid CarId { get; set; }
-        public Car Car { get; set; }
+        [ForeignKey(nameof(CarId))]
+        public Car Car { get; set; } = null!;
+        [Comment("Discount unique identifier")]
         public Guid DiscountId { get; set; }
-        public Discount Discount { get; set; }
+        [ForeignKey(nameof(DiscountId))]
+        public Discount Discount { get; set; } = null!;
+        [Comment("Has the discount been deleted or not")]
+        public bool IsDeleted { get; set; }
     }
 }
