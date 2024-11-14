03d7aced7f9f3eab02ba109920f736d6df4a9baf
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-11-14 14:58:37 +0200

    Added base controller

---
+++ CarShop.Web/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarShop.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected bool IsGuidValid(string id, Guid parsedGuid)
+        {
+            if (String.IsNullOrWhiteSpace(id))
+            {
+                return false;
+            }
+
+            bool isGuidValid = Guid.TryParse(id, out parsedGuid);
+            if (!isGuidValid)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
