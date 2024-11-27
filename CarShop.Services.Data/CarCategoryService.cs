84307af51159aea49c13d8038332bf94181927ee
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-11-27 19:08:45 +0200

    Added car category service

---
+++ CarShop.Services.Data/CarCategoryService.cs
@@ -0,0 +1,34 @@
+﻿using CarShop.Data.Models;
+using CarShop.Data.Repository.Interfaces;
+using CarShop.Services.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.Services.Data
+{
+    public class CarCategoryService : ICarCategoryService
+    {
+        private readonly IRepository<CarCategory, Guid> _categoryRepository;
+
+        public CarCategoryService(IRepository<CarCategory, Guid> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public async Task<List<SelectListItem>> GetCarCategoriesAsync()
+        {
+            return await _categoryRepository
+                .GetAllAttached()
+                .Select(c => new SelectListItem
+                {
+                    Value = c.Id.ToString(),
+                    Text = c.CategoryName
+                })
+                .ToListAsync(); 
+        }
+    }
+}
