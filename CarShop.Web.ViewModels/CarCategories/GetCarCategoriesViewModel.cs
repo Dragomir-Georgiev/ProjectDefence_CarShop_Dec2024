c628d225f380cb470e85ecfbd12b99cbf71a7b8f
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-11-27 19:09:50 +0200

    Added car categories view model

---
+++ CarShop.Web.ViewModels/CarCategories/GetCarCategoriesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.Web.ViewModels.CarCategories
+{
+    public class GetCarCategoriesViewModel
+    {
+        [Required]
+        public Guid CarCategoryId { get; set; }
+        public IEnumerable<SelectListItem> CarCategories { get; set; } = new List<SelectListItem>();
+    }
+}
