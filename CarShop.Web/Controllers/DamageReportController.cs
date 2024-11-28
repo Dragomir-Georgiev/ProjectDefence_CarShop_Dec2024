3c91a4fe9078a2d2d7572a35f920a427fece0f70
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-11-28 19:10:12 +0200

    Updated damage report controller to implement damage report service

--- CarShop.Web/Controllers/DamageReportController.cs
+++ CarShop.Web/Controllers/DamageReportController.cs
@@ -1,5 +1,6 @@
 ﻿using CarShop.Data;
 using CarShop.Data.Models;
+using CarShop.Services.Data.Interfaces;
 using CarShop.Web.ViewModels.DamageReport;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,38 +9,20 @@ namespace CarShop.Web.Controllers
 {
     public class DamageReportController : BaseController
     {
-        private readonly ApplicationDbContext _context;
-        public DamageReportController(ApplicationDbContext context)
+        private readonly IDamageReportService _damageReportService;
+        public DamageReportController(IDamageReportService damageReportService)
         {
-            _context = context;
+            _damageReportService = damageReportService;
         }
 
         [HttpGet]
         public async Task<IActionResult> Index(Guid carId)
         {
-            var car = await _context.Cars
-            .Include(c => c.DamageReports)
-            .FirstOrDefaultAsync(c => c.Id == carId);
-
-            if (car == null)
+            DamageReportIndexViewModel? viewModel = await _damageReportService.GetDamageReportsByCarIdAsync(carId);
+            if (viewModel == null)
             {
-                return this.RedirectToAction("Details", "Car", new { id = carId.ToString() });
+                return RedirectToAction("Details", "Car", new { id = carId.ToString() });
             }
-
-            var viewModel = new DamageReportIndexViewModel()
-            {
-                CarId = car.Id,
-                CarMakeModel = $"{car.Make} {car.Model}",
-                DamageReport = car.DamageReports.Select(dr => new DamageReportViewModel()
-                {
-                    Id = dr.Id,
-                    Description = dr.Description,
-                    ReportedDate = dr.ReportedDate,
-                    CostEstimation = dr.CostEstimation,
-                })
-                .FirstOrDefault()!,
-            };
-            
 
             return View(viewModel);
         }
@@ -52,45 +35,18 @@ namespace CarShop.Web.Controllers
                 return RedirectToAction("Index", new { carId = form.CarId });
             }
 
-            var validCar = await _context.Cars
-                .FirstOrDefaultAsync(c => c.Id == form.CarId);
-            if (validCar == null)
-            {
-                return this.RedirectToAction("Index", "Car");
-            }
-
-            validCar.IsAvailable = false;
-
-            var damageReport = new DamageReport()
-            {
-                CarId = form.CarId,
-                Description = form.Description,
-                ReportedDate = DateTime.Now,
-                CostEstimation = form.CostEstimation,
-            };
-
-            await _context.DamageReports.AddAsync(damageReport);
-            await _context.SaveChangesAsync();
-
+            await _damageReportService.AddDamageReportAsync(form);
             return RedirectToAction("Index", new { carId = form.CarId });
         }
 
         [HttpGet]
         public async Task<IActionResult> Edit(Guid id)
         {
-            var report = await _context.DamageReports.FirstOrDefaultAsync(dr => dr.Id == id);
-            if (report == null)
+            DamageReportFormViewModel? viewModel = await _damageReportService.GetDamageReportForEditAsync(id);
+            if (viewModel == null)
             {
-                return RedirectToAction("Index", new { carId = report!.CarId });
+                return RedirectToAction("Index", new { carId = id });
             }
-
-            var viewModel = new DamageReportFormViewModel
-            {
-                Id = report.Id,
-                CarId = report.CarId,
-                Description = report.Description,
-                CostEstimation = report.CostEstimation,
-            };
 
             return View(viewModel);
         }
@@ -103,66 +59,24 @@ namespace CarShop.Web.Controllers
                 return RedirectToAction("Index", new { carId = form.CarId });
             }
 
-            var report = await _context.DamageReports.FirstOrDefaultAsync(dr => dr.Id == form.Id);
-            if (report == null)
-            {
-                return RedirectToAction("Index", new { carId = form.CarId });
-            }
+            await _damageReportService.EditDamageReportAsync(form);
 
-            report.Description = form.Description;
-            report.CostEstimation = form.CostEstimation;
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction("Index", new { carId = report.CarId });
+            return RedirectToAction("Index", new { carId = form.CarId });
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Remove(Guid id)
         {
-            var report = await _context.DamageReports.FirstOrDefaultAsync(dr => dr.Id == id);
-            if (report == null)
-            {
-                return RedirectToAction("Index", new { carId = report!.CarId });
-            }
+            await _damageReportService.RemoveDamageReportAsync(id);
 
-            var validCar = await _context.Cars
-                .FirstOrDefaultAsync(c => c.Id == report!.CarId);
-            if (validCar == null)
-            {
-                return this.RedirectToAction("Index", "Car");
-            }
-
-            //TODO check if the car has any rentals before making it available
-
-            validCar.IsAvailable = true;
-
-            _context.DamageReports.Remove(report);
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction("Index", new { carId = report.CarId });
+            return RedirectToAction("Index", new { carId = id });
         }
 
         [HttpGet]
         public async Task<IActionResult> All()
         {
-            var carsWithDamageReports = await _context.Cars
-                .Include(c => c.DamageReports)
-                .Where(c => c.DamageReports.Any())
-                .Select(c => new DamageReportIndexViewModel()
-                {
-                    CarId = c.Id,
-                    CarMakeModel = $"{c.Make} {c.Model}",
-                    DamageReport = c.DamageReports.Select(dr => new DamageReportViewModel
-                    {
-                        Id = dr.Id,
-                        Description = dr.Description,
-                        ReportedDate = dr.ReportedDate,
-                        CostEstimation = dr.CostEstimation
-                    }).FirstOrDefault()!
-                })
-                .ToListAsync();
-
+            var carsWithDamageReports = await _damageReportService.GetAllCarsWithDamageReportsAsync();
             return View(carsWithDamageReports);
         }
     }
