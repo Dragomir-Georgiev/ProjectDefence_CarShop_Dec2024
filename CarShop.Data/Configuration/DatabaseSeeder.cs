df638e1ab74ae67710fba7e33ae754e5338df88b
Dragomir-Georgiev <drag.georg@yahoo.ca>
2024-12-05 14:10:43 +0200

    Seeding roles to the db

---
+++ CarShop.Data/Configuration/DatabaseSeeder.cs
@@ -0,0 +1,30 @@
+﻿using CarShop.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CinemaApp.Data.Configuration
+{
+    public static class DatabaseSeeder
+    {
+        public static void SeedRoles(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+            string[] roles = { "Admin", "Manager", "User" };
+
+            foreach (var role in roles)
+            {
+                var roleExists = roleManager.RoleExistsAsync(role).GetAwaiter().GetResult();
+                if (!roleExists)
+                {
+                    var result = roleManager.CreateAsync(new IdentityRole<Guid> { Name = role }).GetAwaiter().GetResult();
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception($"Failed to create role: {role}");
+                    }
+                }
+            }
+        }
+    }
+}
